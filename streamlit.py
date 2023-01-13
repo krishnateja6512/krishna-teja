4d695092e0e0a8b66a352be42c4a75d6c00ac4ef
krishnateja6512 <122591504+krishnateja6512@users.noreply.github.com>
2023-01-12 22:59:22 -0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,6 @@
 import streamlit as st
 st.title("kt")
-st.header("This is a header")
-st.subheader("This is a subheader")
 st.success("hello")
 name = st.text_input("Enter Your name")
 if(st.button('Submit')):
-    result = name.title()
     st.success(result)
